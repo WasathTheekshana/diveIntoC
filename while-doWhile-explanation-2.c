e91b33cdf21cd3d4f4a559b5d90adde72917b7eb
Wasath Theekshana <wasath.vt@gmail.com>
2023-02-23 16:22:55 +0530

    Create while-doWhile-explanation-2.c

---
+++ while-doWhile-explanation-2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main ()
+{
+    int n = 0;
+
+    do
+    {
+        printf("Do you love me ?\n");
+        printf("Yes! of course <3");
+        
+    }while (n > 0);
+
+    return 0;
+}
